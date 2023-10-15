3d0b585d13daa86805b34192b61e6163c6182d9e
omarjafor <omarjaforchy@gmail.com>
2023-10-15 14:52:43 +0600

    added some components

--- src/Components/UpdateCoffee.jsx
+++ src/Components/UpdateCoffee.jsx
@@ -2,8 +2,80 @@
 
 const UpdateCoffee = () => {
     return (
-        <div>
-            
+        <div className="bg-[#F4F3F0] p-24">
+            <h2 className="text-3xl font-extrabold">Update Coffee: {name}</h2>
+            <form onSubmit={handleUpdateCoffee}>
+                {/* form name and quantity row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Coffee Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="name" defaultValue={name} placeholder="Coffee Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Available Quantity</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="quantity" defaultValue={quantity} placeholder="Available Quantity" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form supplier row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Supplier Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="supplier" defaultValue={supplier} placeholder="Supplier Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Taste</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="taste" defaultValue={taste} placeholder="Taste" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form category and details row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Category</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="category" defaultValue={category} placeholder="Category" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Details</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="details" defaultValue={details} placeholder="Details" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form Photo url row */}
+                <div className="mb-8">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="photo" defaultValue={photo} placeholder="Photo URL" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                <input type="submit" value="Update Coffee" className="btn btn-block" />
+
+            </form>
         </div>
     );
 };
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@
 const Home = () => {
     return (
         <div>
-            <div> <h1 className="text-3xl font-bold underline">Hello world!</h1></div>
+            <div> <h1 className="text-3xl font-bold underline">Coffee Store Client</h1></div>
         </div>
     );
 };
