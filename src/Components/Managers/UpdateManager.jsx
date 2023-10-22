4ec5d8038929ca2a00285a995f45bac4fca76a6f
omarjafor <omarjaforchy@gmail.com>
2023-10-22 23:25:13 +0600

    coffee rest practice

--- src/Components/Managers/UpdateManager.jsx
+++ src/Components/Managers/UpdateManager.jsx
@@ -10,6 +10,8 @@ const UpdateManager = () => {
         const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
+        const salary = e.target.salary.value;
+        const date = e.target.date.value;
 
         const updatedManager = { name, email, password, salary, date, details, photo }
 
