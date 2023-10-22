39cb8c08bb2f029a35ae00dba009800398cdfc83
omarjafor <omarjaforchy@gmail.com>
2023-10-22 23:22:59 +0600

    coffee rest practice

--- src/Components/Managers/AddManager.jsx
+++ src/Components/Managers/AddManager.jsx
@@ -7,7 +7,6 @@ const AddManager = () => {
         const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
-        const salary = e.target.salary.value;
 
         const manager = { name, email, password, salary, date, details, photo }
 
